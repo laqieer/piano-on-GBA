0c83a7a7b824bd0555876daf85d05ab5574d3c56
laqieer <laqieer@126.com>
2022-01-03 02:37:11 +0800

    Add a song

--- src/scores.c
+++ src/scores.c
@@ -5,12 +5,14 @@ const char score0[] = INCBIN_S8("scores/LOST_IN_THOUGHTS_ALL_ALONE_from_FIRE_EMB
 const char score1[] = INCBIN_S8("scores/BEYOND_THE_DISTANT_SKIES_from_FIRE_EMBLEM_by_Yuka_Tsujiyoko.txt");
 const char score2[] = INCBIN_S8("scores/FODLAN_NO_GYOUFUU_from_FIRE_EMBLEM_by_Takeru_Kanazaki.txt");
 const char score3[] = INCBIN_S8("scores/THE_EDGE_OF_DAWN_from_FIRE_EMBLEM_THREE_HOUSES_by_Yuka_Tsujiyoko.txt");
+const char score4[] = INCBIN_S8("scores/A_CRUEL_ANGEL_S_THESIS_from_NEON_GENESIS_EVANGELION_by_Yoko_Takahashi.txt");
 
 const int gScoreTempos[] = {
     70,
     125,
     168,
     180,
+    129,
 };
 
 const char * const gScores[] = {
@@ -18,6 +20,7 @@ const char * const gScores[] = {
     score1,
     score2,
     score3,
+    score4,
 };
 
 const int maxSongId = sizeof(gScores) / sizeof(gScores[0]) - 1;
